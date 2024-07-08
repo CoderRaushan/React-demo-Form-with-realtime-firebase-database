ba917173ad724e95717cd198b2696e77b8e42335
CoderRaushanSingh <kumarraushan7631867534@gmail.com>
2024-07-08 10:49:48 +0530

    react form

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React Form/',
+  base: '/React-demo-Form-with-realtime-firebase-database/',
   plugins: [react()],
 })
